ab179cd0edc04718bd182339e86c5d74bc6b760c
tommy <tj2fries@uwaterloo.ca>
2024-02-10 16:02:16 -0500

    Rename

--- Personal/Arts_Preps_Conjs.cpp
+++
@@ -1,9 +0,0 @@
-#include<cstdlib>
-#include<string>
-
-using namespace std;
-
-string smalls[29] = {"a", "an", "the", "for", "and", "nor", "but", "or","yet",
-                      "so", "as", "at", "by", "ere", "in", "of", "off", "on",
-                      "out", "per", "pre", "pro", "qua", "re", "sub", "to",
-                      "up", "via", "ago"};
